15fee72529fb1b909ffa8d919796671d242ed4c5
Dan Hope <44243241+veritasphoenix@users.noreply.github.com>
2023-10-05 10:32:01 -0700

    Update SimpleTwitchBot.py

--- SimpleTwitchBot.py
+++ SimpleTwitchBot.py
@@ -1,7 +1,7 @@
 import concurrent.futures, time, random, os
 
 #desired channel url
-channel_url = 'https://www.twitch.tv/yourchannelname'
+channel_url = 'https://www.twitch.tv/trepixels'
 #number of viewer bots
 botcount = 10
 #path to proxies.txt file
